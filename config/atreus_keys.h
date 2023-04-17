aac6e5354c442062964ea34ad785ecb7f987d619
Adway Malhotra <adway@google.com>
2023-04-17 21:04:45 +0100

    Major refactor to incorporate urob's ideas.

---
+++ config/atreus_keys.h
@@ -0,0 +1,53 @@
+#define KEYS_L 0 1 2 3 4 10 11 12 13 14 20 21 22 23 24 30 31 32
+#define KEYS_R 5 6 7 8 9 15 16 17 18 19 25 26 27 28 29 39 40 41
+#define THUMBS 33 34 35 36 37 38
+
+#define LT0 4
+#define LT1 3
+#define LT2 2
+#define LT3 1
+#define LT4 0
+
+#define RT0 5
+#define RT1 6
+#define RT2 7
+#define RT3 8
+#define RT4 9
+
+#define LM0 14
+#define LM1 13
+#define LM2 12
+#define LM3 11
+#define LM4 10
+
+#define RM0 15
+#define RM1 16
+#define RM2 17
+#define RM3 18
+#define RM4 19
+
+#define LB0 24
+#define LB1 23
+#define LB2 22
+#define LB3 21
+#define LB4 20
+
+#define RB0 25
+#define RB1 26
+#define RB2 27
+#define RB3 28
+#define RB4 29
+
+#define LH0 35
+#define LH1 34
+#define LH2 33
+#define LH3 32
+#define LH4 31
+#define LH5 30
+
+#define RH0 36
+#define RH1 37
+#define RH2 38
+#define RH3 39
+#define RH4 40
+#define RH5 41
---
+++ config/totem_keys.h
@@ -0,0 +1,49 @@
+#define KEYS_L 0 1 2 3 4 10 11 12 13 14 20 21 22 23 24 25
+#define KEYS_R 5 6 7 8 9 15 16 17 18 19 26 27 28 29 30 31
+#define THUMBS 32 33 34 35 36 37
+
+#define LT0 4
+#define LT1 3
+#define LT2 2
+#define LT3 1
+#define LT4 0
+
+#define RT0 5
+#define RT1 6
+#define RT2 7
+#define RT3 8
+#define RT4 9
+
+#define LM0 14
+#define LM1 13
+#define LM2 12
+#define LM3 11
+#define LM4 10
+
+#define RM0 15
+#define RM1 16
+#define RM2 17
+#define RM3 18
+#define RM4 19
+
+#define LB0 25
+#define LB1 24
+#define LB2 23
+#define LB3 22
+#define LB4 21
+#define LB5 20
+
+#define RB0 26
+#define RB1 27
+#define RB2 28
+#define RB3 29
+#define RB4 30
+#define RB5 31
+
+#define LH0 32
+#define LH1 33
+#define LH2 34
+
+#define RH0 35
+#define RH1 36
+#define RH2 37
