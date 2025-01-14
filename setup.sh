237b701da3cdd212d47555e0b2ff5b4eec88a413
Adway Malhotra <adway@google.com>
2025-01-14 12:11:50 +0000

    remove old code from setup.sh

--- setup.sh
+++ setup.sh
@@ -4,37 +4,7 @@
   NIX_FIRST_BUILD_UID=50000 NIX_BUILD_GROUP_ID=50000 sh <(curl -L https://nixos.org/nix/install) --daemon
 
 source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
-nix profile install nixpkgs#direnv nixpkgs#nix-direnv
+nix profile install nixpkgs#direnv nixpkgs#nix-direnv --extra-experimental-features flakes --extra-experimental-features nix-command
 
 direnv allow
 just init
-# start_dir=$PWD
-# sudo apt install python3-venv
-# python3 -m venv .venv
-# source .venv/bin/activate
-#
-# pip install west
-#
-# west init -l config
-# west update
-# west zephyr-export
-#
-# pip install -r zephyr/scripts/requirements-base.txt
-#
-# zephyr_sdk_version="0.16.3"
-# zephyr_sdk_name="zephyr-sdk-${zephyr_sdk_version}"
-# if [ ! -d "$HOME/.local/lib/${zephyr_sdk_name}" ]; then
-#   cd /tmp
-#   wget https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v${zephyr_sdk_version}/${zephyr_sdk_name}_linux-x86_64.tar.xz
-#   wget -O - https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v${zephyr_sdk_version}/sha256.sum | shasum --check --ignore-missing
-#
-#   tar xvf ${zephyr_sdk_name}_linux-x86_64.tar.xz
-#   mv ${zephyr_sdk_name} $HOME/.local/lib/${zephyr_sdk_name}
-#   cd $HOME/.local/lib/${zephyr_sdk_name}
-#   ./setup.sh
-#
-#   sudo cp sysroots/x86_64-pokysdk-linux/usr/share/openocd/contrib/60-openocd.rules /etc/udev/rules.d
-#   sudo udevadm control --reload
-# fi
-#
-# cd $start_dir
