fb147b0352b6c5ef47a8786180dfc0f34e164098
Adway Malhotra <adway@google.com>
2026-03-02 10:26:28 +0000

    updates

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     # This pins requirements.txt provided by zephyr-nix.pythonEnv.
-    zephyr.url = "github:zmkfirmware/zephyr/v4.1.0+zmk-fixes";
+    zephyr.url = "github:zmkfirmware/zephyr/v3.5.0+zmk-fixes";
     zephyr.flake = false;
 
     # Zephyr sdk and toolchain.
@@ -25,7 +25,7 @@
         default = pkgs.mkShellNoCC {
           packages =
             [
-              zephyr.pythonEnv
+              zephyr.pythonEnv               
               (zephyr.sdk-0_16.override {targets = ["arm-zephyr-eabi"];})
 
               pkgs.cmake
