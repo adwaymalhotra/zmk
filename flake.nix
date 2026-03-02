8cc5aafd7e581180e89d012bdc6745474b147bf3
Adway Malhotra <adway@google.com>
2026-03-02 14:15:52 +0000

    Update to Zephyr 4.1 and ZMK main branch.
    - use the cosmos-zmk-module for lemon wireless
    - update the board names to include the `//zmk` suffix. Without this the
      resulting firmware is broken.
    - fix the limoncello keymap indices to work with the 6x4 layout that
      we've got at the moment.

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     # This pins requirements.txt provided by zephyr-nix.pythonEnv.
-    zephyr.url = "github:zmkfirmware/zephyr/v3.5.0+zmk-fixes";
+    zephyr.url = "github:zmkfirmware/zephyr/v4.1.0+zmk-fixes";
     zephyr.flake = false;
 
     # Zephyr sdk and toolchain.
@@ -19,14 +19,17 @@
     devShells = forAllSystems (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
+        python = pkgs.python3Packages.python310;
         zephyr = zephyr-nix.packages.${system};
         keymap_drawer = pkgs.python3Packages.callPackage ./nix/keymap-drawer.nix {};
       in {
         default = pkgs.mkShellNoCC {
           packages =
             [
-              zephyr.pythonEnv
-              (zephyr.sdk-0_16.override {targets = ["arm-zephyr-eabi"];})
+              zephyr.hosttools-nix
+              zephyr.pythonEnv 
+              (zephyr.sdk.override {targets = ["arm-zephyr-eabi"];})
+              # (zephyr.sdk-0_17.override {targets = ["arm-zephyr-eabi"];})
 
               pkgs.cmake
               pkgs.dtc
